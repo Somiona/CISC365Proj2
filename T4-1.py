428e447cf598437a7abfa4b49ba3efe3cfed1472
Somiona <somionat@gmail.com>
2022-11-18 17:08:42 -0500

    initial project

---
+++ T4-1.py
@@ -0,0 +1,50 @@
+"""
+Enter names and Stu# here!
+"""
+
+"""
+Enter your pseudo code here!
+"""
+
+
+def island_hopping(c):
+    """
+    Enter your code here!
+    """
+    
+    return
+                    
+
+"""
+Testing code
+"""
+
+c = (2,15,32,3)
+print(c)
+print("Answer (20, '0-1-3-4')")
+print(island_hopping(c),"\n") 
+
+c = (10,10,40,33,15,1)
+print(c)
+print("Answer (54, '0-1-3-5-6')")
+print(island_hopping(c),"\n")
+
+c = (15,3,11,36)
+print(c)
+print("Answer (26, '0-2-4')")
+print(island_hopping(c),"\n")
+
+c = (15,3,11,36,2,18)
+print(c)
+print("Answer (28, '0-2-4-6')")
+print(island_hopping(c),"\n")
+
+c = (15,3,11,17,36,2,18)
+print(c)
+print("Answer (37, '0-1-3-5-7')")
+print(island_hopping(c),"\n")
+
+c = (5,3,17,6,2,16,1)
+print(c)
+print("Answer (17, '0-1-3-4-6-7')")
+print(island_hopping(c),"\n")
---
+++ T4-2.py
@@ -0,0 +1,63 @@
+"""
+Enter names and Stu# here!
+"""
+
+"""
+Enter your pseudo code here!
+"""
+
+
+def pacman(ghost):
+    """
+    Enter your code here!
+    """
+    
+    return
+
+    
+    
+                    
+
+"""
+Testing code
+"""
+
+ghost=[[0,1],
+       [0,0]]
+print("for input:", ghost)
+print("Answer: 1")
+print(pacman(ghost),"\n")
+
+
+ghost=[[0,1],
+       [0,0]]
+print("for input:", ghost)
+print("Answer: 0")
+print(pacman(ghost),"\n")
+
+
+ghost=[[0,0,0,1],
+       [0,1,0,0],
+       [0,0,0,0],
+       [0,0,0,0]]
+print("for input:", ghost)
+print("Answer: 7")
+print(pacman(ghost),"\n") 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
